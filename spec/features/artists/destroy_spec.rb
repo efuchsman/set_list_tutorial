93c3c95db27d819339641b5bb9ec07b54592e8f1
Eli Fuchsman <efuchsman@gmail.com>
2022-10-15 15:08:39 -0600

    Create abilty to destroy artists

---
+++ spec/features/artists/destroy_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'destroy an artist' do
+  it 'can delete the artist from the index page' do
+    artist= Artist.create!(name: 'Prince')
+
+    visit '/artists'
+    # save_and_open_page
+    click_button 'Delete'
+    expect(current_path).to eq('/artists')
+    expect(page).to_not have_content('Prince')
+  end
+end
