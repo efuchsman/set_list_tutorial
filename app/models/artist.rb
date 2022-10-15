31b0037c24da69ea842271d95a6896169c6cdc90
Eli Fuchsman <efuchsman@gmail.com>
2022-10-15 13:53:20 -0600

    Add create new artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,19 @@
 class Artist < ApplicationRecord
- has_many :songs
+  has_many :songs
 
   def average_song_length
     self.songs.average(:length)
+  end
+
+  # def songs
+  #   Song.where(artist_id: self.id)
+  # end
+
+  def song_count
+    self.songs.count
+  end
+
+  def wrote_song?(song)
+    self.songs.include?(song)
   end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -3,15 +3,31 @@ require 'rails_helper'
 RSpec.describe Artist, type: :model do
   it {should have_many :songs}
   describe 'instance methods' do
+    before :each do
+      @prince = Artist.create!(name: 'Prince')
+      @purple = @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
+      @beret = @prince.songs.create!(title: 'Raspberry Beret', length: 664, play_count: 99)
+      @other_song = @prince.songs.create!(title: 'Another Prince Song', length: 1, play_count: 99)
+    end
     describe 'average_song_length' do
-      before :each do
-        @prince = Artist.create!(name: 'Prince')
-        @purple = @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
-        @beret = @prince.songs.create!(title: 'Raspberry Beret', length: 664, play_count: 99)
-        @other_song = @prince.songs.create!(title: 'Another Prince Song', length: 1, play_count: 99)
-      end
       it 'returns the average song length' do
         expect(@prince.average_song_length.round(2)).to eq(503.33)
+      end
+    end
+    describe '#songs' do
+      it 'returns all of the associated song objects' do
+
+        expect(@prince.songs).to eq([@purple, @beret, @other_song])
+      end
+    end
+    describe '#song_count' do
+      it 'counts the number of songs' do
+        expect(@prince.song_count).to eq(3)
+      end
+    end
+    describe '#wrote_song?' do
+      it 'returns true if the song is associated with the artist' do
+        expect(@prince.wrote_song?(@purple)).to be true
       end
     end
   end
